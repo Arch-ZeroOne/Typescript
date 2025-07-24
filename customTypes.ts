7afeaae37bc5771d84ea52fd40253f1b0f1f7dd7
Windyl Monton <windyl1.monton.lvhs@gmail.com>
2025-07-24 18:28:34 +0800

    Learned about customTypes

---
+++ customTypes.ts
@@ -0,0 +1,30 @@
+// Custom types in TS forces Us to stay consistent when it comes to defining objects in our codebase
+//Custom types in typescript should be in UpperCase
+
+
+
+type Person = {
+  name: string;
+  age: number;
+  isStudent: boolean;
+};
+
+let person: Person = {
+  name: "Joe",
+  age: 42,
+  isStudent: true,
+};
+
+//Typescript will warn if the created object doesnt match the custom type created ex:
+
+// let person2: Person = {
+//   name: "Jill",
+//   age: 22,
+//   isstudent: false, //! Error will show up here
+// };
+
+let person2: Person = {
+  name: "Jill",
+  age: 22,
+  isStudent: false,
+};
--- obligatory.ts
+++ obligatory.ts
@@ -1,2 +1,4 @@
 //assigning the type manually in TS
 let myName: string = "Bob";
+let numberOfWheels: number = 4;
+let isStudent: boolean = false;
